419fdc8a40573dd763c16b883437fb25eeba7ca9
Michael Johnson <mj82@grnoc.iu.edu>
2018-04-06 20:48:19 +0000

    Bump version numer to 3.0.4

--- deploy/docker/build.sh
+++ deploy/docker/build.sh
@@ -4,6 +4,7 @@ echo "preparing mca-admin"
 rm -rf mca-admin/tmp
 mkdir mca-admin/tmp
 cp -r ../../api mca-admin/tmp
+rm -f mca-admin/tmp/api/config.js
 cp -r ../../ui mca-admin/tmp
 cp -r ../../package.json mca-admin/tmp
 rm -rf mca-admin/tmp/api/config
@@ -14,13 +15,14 @@ if [ ! $? -eq 0 ]; then
     exit
 fi
 
-docker tag perfsonar/mca-admin perfsonar/mca-admin:3.0
+docker tag perfsonar/mca-admin perfsonar/mca-admin:3.0.4
 docker push perfsonar/mca-admin
 
 echo "preparing mca-pub"
 rm -rf mca-pub/tmp
 mkdir mca-pub/tmp
 cp -r ../../api mca-pub/tmp
+rm -f mca-pub/tmp/api/config.js
 cp -r ../../package.json mca-pub/tmp
 rm -rf mca-pub/tmp/api/config
 
@@ -30,5 +32,5 @@ if [ ! $? -eq 0 ]; then
     exit
 fi
 
-docker tag perfsonar/mca-pub perfsonar/mca-pub:3.0
+docker tag perfsonar/mca-pub perfsonar/mca-pub:3.0.4
 docker push perfsonar/mca-pub
