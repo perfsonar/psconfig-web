3c49eba5a7ae4841fcc502e39ec5ac90fb4940e1
Michael Johnson <mj82@grnoc.iu.edu>
2018-05-17 17:49:40 +0000

    Update start script #44

--- start.sh
+++ start.sh
@@ -3,13 +3,13 @@
 #run this for production
 #sequelize db:migrate
 
-pm2 delete mcadmin 
-pm2 start api/mcadmin.js --watch --ignore-watch="ui deploy \.log$ test .sh$ api/pub"
+pm2 delete pwaadmin 
+pm2 start api/pwaadmin.js --watch --ignore-watch="ui deploy \.log$ test .sh$ api/pub"
 
-pm2 delete mccache
-pm2 start api/mccache.js --watch --ignore-watch="ui deploy \.log$ test .sh$ api/pub"
+pm2 delete pwacache
+pm2 start api/pwacache.js --watch --ignore-watch="ui deploy \.log$ test .sh$ api/pub"
 
-pm2 delete mcpub
-pm2 start api/mcpub.js --watch --ignore-watch="ui deploy \.log$ test .sh$ api/admin"
+pm2 delete pwapub
+pm2 start api/pwapub.js --watch --ignore-watch="ui deploy \.log$ test .sh$ api/admin"
 
 #pm2 logs meshconfig
