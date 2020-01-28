03b29ff0b4ace1e8affd6222a461045d1dfe154c
Michael Johnson <mj82@grnoc.iu.edu>
2020-01-28 21:31:24 +0000

    Add code changes to go with npm dep upgrade #165

--- api/admin/controllers/hosts.js
+++ api/admin/controllers/hosts.js
@@ -51,7 +51,7 @@ router.get('/', jwt({secret: config.admin.jwt.pub}), function(req, res, next) {
     .lean() //so that I can add _canedit later
     .exec(function(err, hosts) {
         if(err) return next(err);
-        db.Host.count(find).exec(function(err, count) {
+        db.Host.countDocuments(find).exec(function(err, count) {
             if(err) return next(err);
 
             hosts.forEach(function(host) {
@@ -177,8 +177,6 @@ router.put('/:id', jwt({secret: config.admin.jwt.pub}), function(req, res, next)
             host = JSON.parse(JSON.stringify(host));
             host._canedit = canedit(req.user, host);
             res.json(host);
-        }).catch(function(err) {
-            next(err);
         });
     });
 });
--- api/admin/controllers/testspecs.js
+++ api/admin/controllers/testspecs.js
@@ -51,7 +51,7 @@ router.get('/', jwt({secret: config.admin.jwt.pub, credentialsRequired: false}),
     .lean() //so that I can add _canedit later
     .exec(function(err, testspecs) {
         if(err) return next(err);
-        db.Testspec.count(find).exec(function(err, count) { 
+        db.Testspec.countDocuments(find).exec(function(err, count) { 
             if(err) return next(err);
             //set _canedit flag for each specs
             testspecs.forEach(function(testspec) {
@@ -136,8 +136,6 @@ router.put('/:id', jwt({secret: config.admin.jwt.pub}), function(req, res, next)
                 testspec = JSON.parse(JSON.stringify(testspec));
                 testspec._canedit = canedit(req.user, testspec);
                 res.json(testspec);
-            }).catch(function(err) {
-                next(err);
             });
         } 
     }); 
