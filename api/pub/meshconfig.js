eb008eee819f54172fa238114194bc6efefbef87
Michael Johnson <mj82@grnoc.iu.edu>
2020-04-24 17:23:01 +0000

    Create schedule if doesn't exist; change docker version to 4.3.0-slip #175

--- api/pub/meshconfig.js
+++ api/pub/meshconfig.js
@@ -231,6 +231,10 @@ function meshconfig_testspec_to_psconfig( testspec, name, psc_tests, schedules )
 
         test._schedule = sched_key;
 
+        if ( ! ( sched_key in schedules ) ) {
+            schedules[ sched_key ] = {};
+        }
+
 
         // "slip"
                 console.log("TEST", test);
