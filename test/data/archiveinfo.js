76f0107b5553ba76696794e099686d0502790fc2
Michael Johnson <mj82@grnoc.iu.edu>
2020-09-15 15:48:56 +0000

    Change archive tests for proper rabbitmq output format

--- test/data/archiveinfo.js
+++ test/data/archiveinfo.js
@@ -25,15 +25,15 @@ archive_checks.push(
         in:
         {"_id":"5efb409a47117561c498000a","admins":["1"],"desc":"New Testspec","name":"rabbit2","archiver":"rabbitmq","data":{"_url":"amqp://mj82:123@rabbithost.example.org:12345","connection_lifetime":1},"create_date":"2020-06-30T13:39:38.383Z","update_date":"2020-08-18T15:14:44.449Z","__v":0},
     out: 
-        {"rabbit2-5efb409a47117561c498000a":{"archiver":"rabbitmq","data":{"_url":"amqp://mj82:123@rabbithost.example.org:12345","connection_lifetime":1,"schema":2}}}
+        {"rabbit2-5efb409a47117561c498000a":{"archiver":"rabbitmq","data":{"_url":"amqp://mj82:123@rabbithost.example.org:12345","connection-expires":"PT1S","schema":2}}}
     }
     );
-archive_checks.push(        
+archive_checks.push(
     {
         name: "Rabbit SSL with username/pass",
-        in: {"_id":"5ef6106042e7f049ba97671a","admins":["1"],"desc":"New Testspec","name":"config-archive1","archiver":"rabbitmq","data":{"_url":"amqps://rabbithost.example.org:12345","_username":"asdf","_password":"qwerty","exchange_key":"asdf","routing_key":"ere","connection_lifetime":2},"create_date":"2020-06-26T15:12:32.463Z","update_date":"2020-09-01T15:27:27.019Z","__v":0},
+        in: {"_id":"5ef6106042e7f049ba97671a","admins":["1"],"desc":"New Testspec","name":"config-archive1","archiver":"rabbitmq","data":{"_url":"amqps://rabbithost.example.org:12345","_username":"asdf","_password":"qwerty","exchange":"asdf","routing_key":"ere","connection_lifetime":2},"create_date":"2020-06-26T15:12:32.463Z","update_date":"2020-09-01T15:27:27.019Z","__v":0},
     out:
-{"config-archive1-5ef6106042e7f049ba97671a":{"archiver":"rabbitmq","data":{"_url":"amqps://asdf:qwerty@rabbithost.example.org:12345","exchange_key":"asdf","routing_key":"ere","connection_lifetime":2,"schema":2}}}
+{"config-archive1-5ef6106042e7f049ba97671a":{"archiver":"rabbitmq","data":{"_url":"amqps://asdf:qwerty@rabbithost.example.org:12345","exchange":"asdf","routing-key":"ere","connection-expires":"PT2S","schema":2}}}
 }
 );
 
