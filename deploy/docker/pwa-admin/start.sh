314cf27ea95ffec907b2e9ad6dfaf53c0cb13be1
John Thiltges <jthiltges2@unl.edu>
2020-10-16 11:18:14 -0500

    Listen on IPv6 in Docker container

--- deploy/docker/pwa-admin/start.sh
+++ deploy/docker/pwa-admin/start.sh
@@ -4,6 +4,6 @@ pm2 start /app/api/pwaadmin.js #--watch /app/api/config
 pm2 start /app/api/pwacache.js #--watch /app/api/config
 
 echo "starting http-server for ui"
-pm2 start http-server --name ui -- -p 80 -a 0.0.0.0 -d false /app/ui 
+pm2 start http-server --name ui -- -p 80 -a :: -d false /app/ui
 
 pm2 logs
