c4b4d8ed0077efe25a83d810cf354f3f1c5c8f89
John Grigutis <grigutis@globalnoc.iu.edu>
2021-06-22 19:45:58 +0000

    Changes to get dev environment working

--- start.sh
+++ start.sh
@@ -4,12 +4,12 @@
 #sequelize db:migrate
 
 pm2 delete pwaadmin 
-pm2 start api/pwaadmin.js --watch --ignore-watch="ui deploy \.log$ test .sh$ api/pub"
+NODE_PATH=/home/grigutis/src/psconfig-web/node_modules pm2 start api/pwaadmin.js --watch --ignore-watch="ui deploy \.log$ test .sh$ api/pub"
 
 pm2 delete pwacache
-pm2 start api/pwacache.js --watch --ignore-watch="ui deploy \.log$ test .sh$ api/pub"
+NODE_PATH=/home/grigutis/src/psconfig-web/node_modules pm2 start api/pwacache.js --watch --ignore-watch="ui deploy \.log$ test .sh$ api/pub"
 
 pm2 delete pwapub
-pm2 start api/pwapub.js --watch --ignore-watch="ui deploy \.log$ test .sh$ api/admin"
+NODE_PATH=/home/grigutis/src/psconfig-web/node_modules pm2 start api/pwapub.js --watch --ignore-watch="ui deploy \.log$ test .sh$ api/admin"
 
 #pm2 logs meshconfig
