822a5d580c053ddf8df1144ecc2fbaed24ecf304
John Grigutis <grigutis@globalnoc.iu.edu>
2022-01-13 20:05:35 +0000

    Adds log messages for clarity.

--- deploy/docker/pwa-mongo/downgrade_mongodb.js
+++ deploy/docker/pwa-mongo/downgrade_mongodb.js
@@ -1,20 +1,14 @@
 // Downgrades MongoDB 4.2 Standalone to 4.0
 // For use with Docker installations of pSConfig Web Admin (PWA) < 5.0
 
+print("*** Starting downgrade_mongodb.js ***");
+
 let ver = db.adminCommand({ getParameter: 1, featureCompatibilityVersion: 1 });
 
 if (ver.featureCompatibilityVersion.version == 4.2) {
-    // Possibly wait until any indexing operations are finished?
-    let indexingOps = db.adminCommand({
-        currentOp: true,
-        $or: [
-            { op: "command", "command.createIndexes": { $exists: true } },
-            { op: "none", msg: /^Index Build/ },
-        ],
-    });
-
     // https://docs.mongodb.com/manual/release-notes/4.2-downgrade-standalone/#1.-downgrade-feature-compatibility-version--fcv-
     db.adminCommand({ setFeatureCompatibilityVersion: "4.0" });
+    print("FeatureCompatibilityVersion set to 4.0");
 
     db.getCollectionNames().forEach(function (collection) {
         let indexes = db[collection].getIndexes();
@@ -28,7 +22,6 @@ if (ver.featureCompatibilityVersion.version == 4.2) {
                         collection +
                         " exceeds 1024 bytes!\nhttps://docs.mongodb.com/manual/release-notes/4.2-downgrade-standalone/#2a.-index-key-size"
                 );
-                // need to exit here
             }
 
             // https://docs.mongodb.com/manual/release-notes/4.2-downgrade-standalone/#2b.-index-name-length
@@ -39,7 +32,6 @@ if (ver.featureCompatibilityVersion.version == 4.2) {
                         fqin +
                         " exceeds 127 bytes!\nhttps://docs.mongodb.com/manual/release-notes/4.2-downgrade-standalone/#2b.-index-name-length"
                 );
-                // need to exit here
             }
         });
     });
@@ -76,35 +68,27 @@ if (ver.featureCompatibilityVersion.version == 4.2) {
     db.getSiblingDB("admin")
         .getCollection("system.users")
         .dropIndex("user_1_db_1");
+    print("Removed user_1_db_1 system unique index");
 
     // https://docs.mongodb.com/manual/release-notes/4.2-downgrade-standalone/#2e.-remove-wildcard-indexes
-    if (indexingOps.inprog.length != 0) {
-        print("I'm in the middle of indexing something. Come back later.");
-        // need to exit here
-    } else {
-        db.adminCommand("listDatabases").databases.forEach(function (d) {
-            let mdb = db.getSiblingDB(d.name);
-            mdb.getCollectionInfos({ type: "collection" }).forEach(function (
-                c
-            ) {
-                let currentCollection = mdb.getCollection(c.name);
-                currentCollection.getIndexes().forEach(function (idx) {
-                    var key = Object.keys(idx.key);
-                    if (key[0].includes("$**")) {
-                        print(
-                            "Dropping index: " + idx.name + " from " + idx.ns
-                        );
+    db.adminCommand("listDatabases").databases.forEach(function (d) {
+        let mdb = db.getSiblingDB(d.name);
+        mdb.getCollectionInfos({ type: "collection" }).forEach(function (c) {
+            let currentCollection = mdb.getCollection(c.name);
+            currentCollection.getIndexes().forEach(function (idx) {
+                var key = Object.keys(idx.key);
+                if (key[0].includes("$**")) {
+                    print("Dropping index: " + idx.name + " from " + idx.ns);
 
-                        let res = mdb.runCommand({
-                            dropIndexes: currentCollection,
-                            index: idx.name,
-                        });
-                        assert.commandWorked(res);
-                    }
-                });
+                    let res = mdb.runCommand({
+                        dropIndexes: currentCollection,
+                        index: idx.name,
+                    });
+                    assert.commandWorked(res);
+                }
             });
         });
-    }
+    });
 
     // https://docs.mongodb.com/manual/release-notes/4.2-downgrade-standalone/#2f.-view-definitions-collection-validation-definitions-that-include-4.2-operators
     // Skipping
@@ -112,6 +96,10 @@ if (ver.featureCompatibilityVersion.version == 4.2) {
     // https://docs.mongodb.com/manual/release-notes/4.2-downgrade-standalone/#5.-remove-client-side-field-level-encryption-document-validation-keywords
     // Skipping
     // db.runComand({ "collMod" : "<collection>", "validator" : {} })
+
+    print("Downgrade complete! You can now use the mongo:4.0.3 docker image.");
 } else {
-    print("MongoDB not downgraded: (featureCompatibilityVersion != 4.2)");
+    print("MongoDB not downgraded: featureCompatibilityVersion != 4.2");
 }
+
+print("*** Finished downgrade_mongodb.js ***");
