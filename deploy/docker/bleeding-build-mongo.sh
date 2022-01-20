c31a78c723ce824d80df931346a6b8ea4b91b73f
John Grigutis <grigutis@globalnoc.iu.edu>
2022-01-20 20:59:12 +0000

    Fixes Docker image building

--- deploy/docker/bleeding-build-mongo.sh
+++ deploy/docker/bleeding-build-mongo.sh
@@ -3,7 +3,7 @@ VERSION=`cat bleeding-version`
 
 echo "preparing pwa-mongo version $VERSION"
 
-docker build pwa-mongo -t perfsonar/pwa-mongo:$VERSION --no-cache --force-rm
+docker build pwa-mongo --network host -t perfsonar/pwa-mongo:$VERSION --no-cache --force-rm
 
 if [ ! $? -eq 0 ]; then
     echo "failed to build"
