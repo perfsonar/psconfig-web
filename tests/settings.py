cdd8f9754b9a42a2f3a5395970921f349d6f9348
John Grigutis <grigutis@globalnoc.iu.edu>
2022-08-09 20:44:49 +0000

    Sets tests to use PostgreSQL instead of SQLite.

--- tests/settings.py
+++ tests/settings.py
@@ -1,7 +1,7 @@
 """Settings module for test app."""
 ENV = "development"
 TESTING = True
-SQLALCHEMY_DATABASE_URI = "sqlite://"
+SQLALCHEMY_DATABASE_URI = "postgresql://psconfig:psconfig@localhost:5432/psconfig-test"
 SECRET_KEY = "not-so-secret-in-tests"
 BCRYPT_LOG_ROUNDS = (
     4  # For faster tests; needs at least 4 to avoid "ValueError: Invalid rounds"
