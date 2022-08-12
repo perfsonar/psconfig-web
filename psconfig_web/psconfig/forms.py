603cc38a7c5d003d2ca4739d46092bd363679445
John Grigutis <grigutis@globalnoc.iu.edu>
2022-08-12 20:54:32 +0000

    Adds a basic hosts page.

--- psconfig_web/psconfig/forms.py
+++ psconfig_web/psconfig/forms.py
@@ -1,2 +1,75 @@
 # -*- coding: utf-8 -*-
 """pSConfig forms."""
+from flask_wtf import FlaskForm
+from wtforms import BooleanField, Field, StringField, widgets
+from wtforms.validators import DataRequired
+
+from .models import Host
+
+
+class TagListField(Field):
+    """A comma-separated list of tags."""
+
+    widget = widgets.TextInput()
+
+    def _value(self):
+        if self.data:
+            return ", ".join(self.data)
+        else:
+            return ""
+
+    def process_formdata(self, valuelist):
+        """."""
+        if valuelist:
+            self.data = [x.strip() for x in valuelist[0].split(",")]
+        else:
+            self.data = []
+
+
+class BetterTagListField(TagListField):
+    """A better comma-separated list of tags."""
+
+    def __init__(self, label=None, validators=None, remove_duplicates=True, **kwargs):
+        """."""
+        super(BetterTagListField, self).__init__(label, validators, **kwargs)
+        self.remove_duplicates = remove_duplicates
+
+    def process_formdata(self, valuelist):
+        """."""
+        super(BetterTagListField, self).process_formdata(valuelist)
+        if self.remove_duplicates:
+            self.data = list(self._remove_duplicates(self.data))
+
+    @classmethod
+    def _remove_duplicates(cls, seq):
+        """Remove duplicates in a case insensitive, but case preserving manner."""
+        d = {}
+        for item in seq:
+            if item.lower() not in d:
+                d[item.lower()] = True
+                yield item
+
+
+class HostForm(FlaskForm):
+    """Host form."""
+
+    name = StringField("Name", validators=[DataRequired()])
+    tags = BetterTagListField("Tags")
+    no_agent = BooleanField("No agent")
+    disabled = BooleanField("Disabled")
+
+    def __init__(self, *args, **kwargs):
+        """Create instance."""
+        super(HostForm, self).__init__(*args, **kwargs)
+        self.host = None
+
+    def validate(self):
+        """Validate the form."""
+        initial_validation = super(HostForm, self).validate()
+        if not initial_validation:
+            return False
+        host = Host.query.filter_by(name=self.name.data).first()
+        if host:
+            self.name.errors.append("Host already exists")
+            return False
+        return True
--- psconfig_web/psconfig/views.py
+++ psconfig_web/psconfig/views.py
@@ -1,23 +1,47 @@
 # -*- coding: utf-8 -*-
-"""Template views."""
-from flask import Blueprint, render_template
+"""pSConfig views."""
+from flask import Blueprint, flash, redirect, render_template, request, url_for
 from flask_login import login_required
+
+from psconfig_web.psconfig.forms import HostForm
+from psconfig_web.psconfig.models import Host
+from psconfig_web.utils import flash_errors
 
 from .models import *  # noqa
 
-blueprint = Blueprint(
-    "template", __name__, url_prefix="/templates", static_folder="../static"
-)
+blueprint = Blueprint("psconfig", __name__, static_folder="../static")
 
 
-@blueprint.route("/")
+# @blueprint.route("/")
+# @login_required
+# def templates():
+#     """List templates."""
+#     return render_template("psconfig/templates.html")
+
+
+# @blueprint.route("/<template>")
+# def display_json():
+#     """Show the template in JSON."""
+#     return render_template("psconfig/templates.html")
+
+
+@blueprint.route("/hosts/", methods=["GET", "POST"])
 @login_required
-def templates():
-    """List templates."""
-    return render_template("psconfig/templates.html")
+def hosts():
+    """List and add hosts."""
 
+    hosts = Host.query.all()
 
-@blueprint.route("/<template>")
-def display_json():
-    """Show the template in JSON."""
-    return render_template("psconfig/templates.html")
+    form = HostForm(request.form)
+    if form.validate_on_submit():
+        Host.create(
+            name=form.name.data,
+            tags=form.tags.data,
+            no_agent=form.no_agent.data,
+            disabled=form.disabled.data,
+        )
+        flash("Host added.", "success")
+        return redirect(url_for("psconfig.hosts"))
+    else:
+        flash_errors(form)
+    return render_template("psconfig/hosts.html", form=form, hosts=hosts)
