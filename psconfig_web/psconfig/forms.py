047f59c7604e58cbacb198bc77c37c7c01c0164f
John Grigutis <grigutis@globalnoc.iu.edu>
2022-08-17 16:29:31 +0000

    Adds basic pages for data input.

--- psconfig_web/psconfig/forms.py
+++ psconfig_web/psconfig/forms.py
@@ -1,10 +1,17 @@
 # -*- coding: utf-8 -*-
 """pSConfig forms."""
 from flask_wtf import FlaskForm
-from wtforms import BooleanField, Field, StringField, widgets
-from wtforms.validators import DataRequired
+from wtforms import (
+    BooleanField,
+    Field,
+    IntegerField,
+    StringField,
+    TextAreaField,
+    widgets,
+)
+from wtforms.validators import DataRequired, IPAddress
 
-from .models import Host
+from .models import Address, Archive, Group, Host, Task, Test
 
 
 class TagListField(Field):
@@ -50,12 +57,101 @@ class BetterTagListField(TagListField):
                 yield item
 
 
+class AddressForm(FlaskForm):
+    """Address form."""
+
+    name = StringField("Name", validators=[DataRequired()])
+    address1 = StringField(
+        "Address",
+        validators=[
+            DataRequired(),
+            IPAddress(ipv6=True, message="Not a valid IP address"),
+        ],
+    )
+    labels = TextAreaField("Labels")
+    remote_addresses = TextAreaField("Remote Addresses")
+    lead_bind_address = StringField("Lead Bind Address")
+    pscheduler_address = StringField("pScheduler Address")
+    tags = BetterTagListField("Tags")
+    no_agent = BooleanField("No Agent")
+    disabled = BooleanField("Disabled")
+
+    def __init__(self, *args, **kwargs):
+        """Create instance."""
+        super(AddressForm, self).__init__(*args, **kwargs)
+        self.address = None
+
+    def validate(self):
+        """Validate the form."""
+        initial_validation = super(AddressForm, self).validate()
+        if not initial_validation:
+            return False
+        address = Address.query.filter_by(name=self.name.data).first()
+        if address:
+            self.name.errors.append("Address already exists")
+            return False
+        return True
+
+
+class ArchiveForm(FlaskForm):
+    """Archive form."""
+
+    name = StringField("Name", validators=[DataRequired()])
+    archiver = StringField("Archiver", validators=[DataRequired()])
+    data = TextAreaField("Data")
+    transform = TextAreaField("Transform")
+    ttl = StringField("TTL")
+
+    def __init__(self, *args, **kwargs):
+        """Create instance."""
+        super(ArchiveForm, self).__init__(*args, **kwargs)
+        self.archive = None
+
+    def validate(self):
+        """Validate the form."""
+        initial_validation = super(ArchiveForm, self).validate()
+        if not initial_validation:
+            return False
+        archive = Archive.query.filter_by(name=self.name.data).first()
+        if archive:
+            self.name.errors.append("Archive already exists")
+            return False
+        return True
+
+
+class GroupForm(FlaskForm):
+    """Group form."""
+
+    name = StringField("Name", validators=[DataRequired()])
+    default_address_label = StringField("Default Address Label")
+    unidirectional = BooleanField("Unidirectional")
+    type = StringField("Type")
+    excludes_self = StringField("Excludes Self")
+    excludes = TextAreaField("Excludes")
+
+    def __init__(self, *args, **kwargs):
+        """Create instance."""
+        super(GroupForm, self).__init__(*args, **kwargs)
+        self.group = None
+
+    def validate(self):
+        """Validate the form."""
+        initial_validation = super(GroupForm, self).validate()
+        if not initial_validation:
+            return False
+        group = Group.query.filter_by(name=self.name.data).first()
+        if group:
+            self.name.errors.append("Group already exists")
+            return False
+        return True
+
+
 class HostForm(FlaskForm):
     """Host form."""
 
     name = StringField("Name", validators=[DataRequired()])
     tags = BetterTagListField("Tags")
-    no_agent = BooleanField("No agent")
+    no_agent = BooleanField("No Agent")
     disabled = BooleanField("Disabled")
 
     def __init__(self, *args, **kwargs):
@@ -71,5 +167,56 @@ class HostForm(FlaskForm):
         host = Host.query.filter_by(name=self.name.data).first()
         if host:
             self.name.errors.append("Host already exists")
+            return False
+        return True
+
+
+class TaskForm(FlaskForm):
+    """Task form."""
+
+    name = StringField("Name", validators=[DataRequired()])
+    scheduled_by = IntegerField("Scheduled By")
+    tools = BetterTagListField("Tools")
+    disabled = BooleanField("Disabled")
+    priority = IntegerField("Priority")
+    reference = TextAreaField("Reference")
+
+    def __init__(self, *args, **kwargs):
+        """Create instance."""
+        super(TaskForm, self).__init__(*args, **kwargs)
+        self.task = None
+
+    def validate(self):
+        """Validate the form."""
+        initial_validation = super(TaskForm, self).validate()
+        if not initial_validation:
+            return False
+        task = Task.query.filter_by(name=self.name.data).first()
+        if task:
+            self.name.errors.append("Task already exists")
+            return False
+        return True
+
+
+class TestForm(FlaskForm):
+    """Test form."""
+
+    name = StringField("Name", validators=[DataRequired()])
+    type = StringField("Type")
+    spec = TextAreaField("Spec")
+
+    def __init__(self, *args, **kwargs):
+        """Create instance."""
+        super(TestForm, self).__init__(*args, **kwargs)
+        self.test = None
+
+    def validate(self):
+        """Validate the form."""
+        initial_validation = super(TestForm, self).validate()
+        if not initial_validation:
+            return False
+        task = Test.query.filter_by(name=self.name.data).first()
+        if task:
+            self.name.errors.append("Test already exists")
             return False
         return True
--- psconfig_web/psconfig/views.py
+++ psconfig_web/psconfig/views.py
@@ -3,8 +3,15 @@
 from flask import Blueprint, flash, redirect, render_template, request, url_for
 from flask_login import login_required
 
-from psconfig_web.psconfig.forms import HostForm
-from psconfig_web.psconfig.models import Host
+from psconfig_web.psconfig.forms import (
+    AddressForm,
+    ArchiveForm,
+    GroupForm,
+    HostForm,
+    TaskForm,
+    TestForm,
+)
+from psconfig_web.psconfig.models import Address, Archive, Group, Host, Task, Test
 from psconfig_web.utils import flash_errors
 
 from .models import *  # noqa
@@ -23,6 +30,80 @@ blueprint = Blueprint("psconfig", __name__, static_folder="../static")
 # def display_json():
 #     """Show the template in JSON."""
 #     return render_template("psconfig/templates.html")
+
+
+@blueprint.route("/addresses/", methods=["GET", "POST"])
+@login_required
+def addresses():
+    """List and add addresses."""
+
+    addresses = Address.query.all()
+
+    form = AddressForm(request.form)
+    if form.validate_on_submit():
+        Address.create(
+            name=form.name.data,
+            address=form.address1.data,
+            labels=form.labels.data,
+            remote_addresses=form.remote_addresses.data,
+            lead_bind_address=form.lead_bind_address.data,
+            pscheduler_address=form.pscheduler_address.data,
+            tags=form.tags.data,
+            no_agent=form.no_agent.data,
+            disabled=form.disabled.data,
+        )
+        flash("Address added.", "success")
+        return redirect(url_for("psconfig.addresses"))
+    else:
+        flash_errors(form)
+    return render_template("psconfig/addresses.html", form=form, addresses=addresses)
+
+
+@blueprint.route("/archives/", methods=["GET", "POST"])
+@login_required
+def archives():
+    """List and add archives."""
+
+    archives = Archive.query.all()
+
+    form = ArchiveForm(request.form)
+    if form.validate_on_submit():
+        Archive.create(
+            name=form.name.data,
+            archiver=form.archiver.data,
+            data=form.data.data,
+            transform=form.transform.data,
+            ttl=form.ttl.data,
+        )
+        flash("Archive added.", "success")
+        return redirect(url_for("psconfig.archives"))
+    else:
+        flash_errors(form)
+    return render_template("psconfig/archives.html", form=form, archives=archives)
+
+
+@blueprint.route("/groups/", methods=["GET", "POST"])
+@login_required
+def groups():
+    """List and add groups."""
+
+    groups = Group.query.all()
+
+    form = GroupForm(request.form)
+    if form.validate_on_submit():
+        Group.create(
+            name=form.name.data,
+            default_address_label=form.default_address_label.data,
+            unidirectional=form.unidirectional.data,
+            type=form.type.data,
+            excludes_self=form.excludes_self.data,
+            excludes=form.excludes.data,
+        )
+        flash("Group added.", "success")
+        return redirect(url_for("psconfig.groups"))
+    else:
+        flash_errors(form)
+    return render_template("psconfig/groups.html", form=form, groups=groups)
 
 
 @blueprint.route("/hosts/", methods=["GET", "POST"])
@@ -45,3 +126,48 @@ def hosts():
     else:
         flash_errors(form)
     return render_template("psconfig/hosts.html", form=form, hosts=hosts)
+
+
+@blueprint.route("/tasks/", methods=["GET", "POST"])
+@login_required
+def tasks():
+    """List and add tasks."""
+
+    tasks = Task.query.all()
+
+    form = TaskForm(request.form)
+    if form.validate_on_submit():
+        Task.create(
+            name=form.name.data,
+            scheduled_by=form.scheduled_by.data,
+            tools=form.tools.data,
+            priority=form.priority.data,
+            reference=form.reference.data,
+            disabled=form.disabled.data,
+        )
+        flash("Task added.", "success")
+        return redirect(url_for("psconfig.tasks"))
+    else:
+        flash_errors(form)
+    return render_template("psconfig/tasks.html", form=form, tasks=tasks)
+
+
+@blueprint.route("/tests/", methods=["GET", "POST"])
+@login_required
+def tests():
+    """List and add tests."""
+
+    tests = Test.query.all()
+
+    form = TestForm(request.form)
+    if form.validate_on_submit():
+        Test.create(
+            name=form.name.data,
+            type=form.type.data,
+            spec=form.spec.data,
+        )
+        flash("Test added.", "success")
+        return redirect(url_for("psconfig.tests"))
+    else:
+        flash_errors(form)
+    return render_template("psconfig/tests.html", form=form, tests=tests)
